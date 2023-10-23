0ae313bd60284749e84309ff99c57e120a4b3094
somethingelse567 <vito.tomas205@gmail.com>
2023-10-23 09:32:34 +0200

    initial: Added dev files

---
+++ IzvorniKod/MP7-progi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MP7_progi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
