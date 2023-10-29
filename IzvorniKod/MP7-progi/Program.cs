411d1ddeaf8b20de032c35636e60cf6d24b2c2e4
somethingelse567 <vito.tomas205@gmail.com>
2023-10-29 19:16:48 +0100

    Added ReactJS support
    Added ReactJS support for .NET CORE MVC application. Test file is
    'ReactTest.jsx' in ~/js/ directory. It is dependent on next NuGet
    packages which you have to install:
    
    * JavaScriptEngineSwitcher.Extensions.MsDependencyInjection
    * JavaScriptEngineSwitcher.V8
    * JavaScriptEngineSwitcher.V8.Native.win-x64
    * JavaScriptEngineSwitcher.V8.Native.win-x86
    * React.AspNet
    
    Do not tamper with script links in the 'head' section of the
    HTML since it is a dependency for ReactJS.

--- IzvorniKod/MP7-progi/Program.cs
+++ IzvorniKod/MP7-progi/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+using System.Collections.Generic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddMvc();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName).AddV8();
+
 var app = builder.Build();
+
+app.UseReact(config =>
+{
+});
+app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ IzvorniKod/MP7-progi/wwwroot/js/ReactTest.jsx
@@ -0,0 +1,9 @@
+﻿class ReactTest extends React.Component {
+    render() {
+        return (
+            <div className="comment">Hello World!</div>
+        );
+    }
+}
+
+ReactDOM.render(<ReactTest />, document.getElementById('root'));
