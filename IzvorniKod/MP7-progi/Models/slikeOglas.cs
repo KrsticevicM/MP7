6e12f3d2b84542fd1b7a92a9b74a0f3b5c9d86bf
KrsticevicM <mk54058@fer.hr>
2023-11-02 20:15:21 +0100

    Added model for photos from ads

---
+++ IzvorniKod/MP7-progi/Models/slikeOglas.cs
@@ -0,0 +1,12 @@
+﻿namespace MP7_progi.Models
+{
+    public class slikeOglas
+    {
+        public int fotoID { get; set; }
+        public int userID { get; set; }
+        public int oglasID { get; set; }
+        public int petID { get; set; }
+        public string foto { get; set; }
+        
+    }
+}
