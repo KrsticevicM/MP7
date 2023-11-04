827b25145253bb262bad0ffe4c64548e1325896b
filipsmolic <filip.smolic.zadar@gmail.com>
2023-11-04 11:51:07 +0100

    Home page files
    Added files for previous commit

---
+++ IzvorniKod/MP7-progi/wwwroot/js/Home.jsx
@@ -0,0 +1,30 @@
+import Ad_card from './Ad_card.jsx';
+import ListGroup from './ListGroup.jsx';
+
+function Home() {
+    return (
+        <div className='home-container'>
+            <div className='left-categories'>
+                <h1 className='search-heading'>Pretraživanje</h1>
+                <div className='categories-container'>
+                    <ListGroup />
+                </div>
+            </div>
+            <div className='ads-container'>
+                <div className='ads-container2'>
+                    <Ad_card name="Johnny" image="\images\Bichon-frise-dog.webp" />
+                    <Ad_card name="Timmy" image="\images\black-dog-breeds-black-labrador-retriever-1566497968.jpg" />
+                    <Ad_card name="Samuel" image="\images\dog-puppy-on-garden-royalty-free-image-1586966191.jpg" />
+                    <Ad_card name="Willy" image="\images\gettyimages-1190158957-1040x690.jpg" />
+                    <Ad_card name="Philip" image="\images\preuzmi.jpg" />
+                    <Ad_card name="Cillian" image="\images\images (1).jpg" />
+                    <Ad_card name="Barthy" image="\images\PomeranianKardashianpoms.jpg" />
+                    <Ad_card name="Victor" image="\images\Portrait-of-a-brown-lagotto-romagnolo.jpg" />
+                    <Ad_card name="Robert" image="\images\images.jpg" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ IzvorniKod/MP7-progi/wwwroot/js/ListGroup.jsx
@@ -0,0 +1,22 @@
+function ListGroup() {
+    return (
+        <>
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <label className="Animal-type">Vrsta ljubimca</label>
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a className="dropdown-item" href="#">Pas</a>
+                    <a className="dropdown-item" href="#">Maèka</a>
+                    <a className="dropdown-item" href="#">Papiga</a>
+                </div>
+            </div>
+            <label className="pet-name">
+                Ime ljubimca: <input name="myInput" />
+            </label>
+            <label>Datum i vrijeme nestanka:<input type="datetime-local" name="datetime" /></label>
+        </>
+    );
+}
+
+export default ListGroup;
