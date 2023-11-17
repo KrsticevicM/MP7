b9a80f898712fb528d7853548bf244d9cce33d4e
Fran Kufrin <fk54068@fer.hr>
2023-11-18 00:05:40 +0100

    deployment changes

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
